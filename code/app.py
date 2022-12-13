ac45415e6884eadbb56503fd5762be1dd365cdbc
Tomas Dratva <4tomas.dratva@gmail.com>
2022-12-13 20:18:04 +0100

    další print řádek

--- code/app.py
+++ code/app.py
@@ -1,9 +1,8 @@
 def pozdrav(koho: str) -> None:
   print(f"Ahoj {koho} ty muj kamarade.")
-
+  print(f"Ahoj {koho} ty muj nejlepsi kamarade.")
 def main():
   pozdrav("Pavle")
 
 if __ame__ == "__main__":
   main()
-  
